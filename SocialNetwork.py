c579c28dbc5f1b9a8a2f538b572a413db99a54ba
asaf <asaf0604@gmail.com>
2024-02-28 04:05:50 +0200

    jmki

--- SocialNetwork.py
+++ SocialNetwork.py
@@ -42,4 +42,4 @@ class SocialNetwork:
         result = f"{self.__name_of_app} social network:\n"
         for name, user in self.__my_dict.items():
             result += f"User name: {name}, Number of posts: {len(user.posts)}, Number of followers: {len(user.followersList)}\n"
-        return result.rstrip()
+        return result
